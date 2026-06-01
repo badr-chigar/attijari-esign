8db3abf3c8d974a51e896f9bb453d4cb6d055151
Badr Chigar <132501656+badr-chigar@users.noreply.github.com>
2026-06-01 15:58:39 +0100

    Add files via upload

---
+++ otp-service/src/main/java/ma/attijari/otp/OtpServiceApplication.java
@@ -0,0 +1,11 @@
+package ma.attijari.otp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtpServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OtpServiceApplication.class, args);
+    }
+}
