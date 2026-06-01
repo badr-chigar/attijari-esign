2ab63782c0edc8618e324fbb9042b17ec61f774e
Badr Chigar <132501656+badr-chigar@users.noreply.github.com>
2026-06-01 16:01:31 +0100

    Add files via upload

---
+++ signature-service/src/main/java/ma/attijari/signature/SignatureServiceApplication.java
@@ -0,0 +1,11 @@
+package ma.attijari.signature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignatureServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SignatureServiceApplication.class, args);
+    }
+}
